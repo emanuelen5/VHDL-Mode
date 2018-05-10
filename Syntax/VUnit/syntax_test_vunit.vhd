225f4ad0dbbd39d6e5a4b99dc86527a2d9c96bfc
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-05-10 15:37:11 +0200

    Adding test for the run keyword

--- Syntax/VUnit/syntax_test_vunit.vhd
+++ Syntax/VUnit/syntax_test_vunit.vhd
@@ -3,11 +3,15 @@
 
 architecture arch of syntax_test_vunit is
 begin
-    run("This should be indexed");
---  ^ source.vhdl meta.block.architecture.body.vhdl support.function.test-case.vunit
---     ^ source.vhdl meta.block.architecture.body.vhdl punctuation.group.parens.begin.vhdl
---       ^ source.vhdl meta.block.architecture.body.vhdl string.double.quoted.vunit.vhdl, entity.test-case.vunit.vhdl
---                             ^ source.vhdl meta.block.architecture.body.vhdl string.double.quoted.vunit.vhdl
---                              ^ source.vhdl meta.block.architecture.body.vhdl punctuation.group.parens.end.vhdl
---                               ^ source.vhdl meta.block.architecture.body.vhdl
+    tests : process
+    begin
+        run("This should be indexed");
+        -- <- support.function.test-case.vunit meta.function-call.procedure.vhdl variable.function.vhdl
+        -- ^ punctuation.group.parens.begin.vhdl
+        --  ^ punctuation.definition.string.begin.vhdl
+        --   ^ entity.test-case.vunit.vhdl meta.function-call.procedure.vhdl
+        --                         ^ punctuation.definition.string.end.vhdl
+        --                          ^ punctuation.group.parens.end.vhdl
+        --                           ^ punctuation.terminator.vhdl
+    end process tests;
 end architecture arch;
